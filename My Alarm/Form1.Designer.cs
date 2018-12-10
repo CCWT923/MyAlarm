cdaf53ff8676f8f03ae42bfe2e9f8f25d6929fdd
WuTao <ccwutao@gmail.com>
2018-12-10 23:41:55 +0800

    新建项目

--- My Alarm/Form1.Designer.cs
+++ My Alarm/Form1.Designer.cs
@@ -30,8 +30,8 @@
         {
             this.FPanel_ToolBox = new System.Windows.Forms.FlowLayoutPanel();
             this.Btn_AddAlarm = new System.Windows.Forms.Button();
-            this.Btn_DeleteAlarm = new System.Windows.Forms.Button();
             this.Btn_EditAlarm = new System.Windows.Forms.Button();
+            this.Btn_DeleteAlarm = new System.Windows.Forms.Button();
             this.FPanel_ToolBox.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -43,49 +43,54 @@
             this.FPanel_ToolBox.Dock = System.Windows.Forms.DockStyle.Top;
             this.FPanel_ToolBox.Location = new System.Drawing.Point(0, 0);
             this.FPanel_ToolBox.Name = "FPanel_ToolBox";
-            this.FPanel_ToolBox.Size = new System.Drawing.Size(911, 47);
+            this.FPanel_ToolBox.Size = new System.Drawing.Size(911, 55);
             this.FPanel_ToolBox.TabIndex = 0;
             // 
             // Btn_AddAlarm
             // 
             this.Btn_AddAlarm.FlatAppearance.BorderSize = 0;
             this.Btn_AddAlarm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_AddAlarm.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_AddAlarm.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.Btn_AddAlarm.Location = new System.Drawing.Point(3, 3);
             this.Btn_AddAlarm.Name = "Btn_AddAlarm";
-            this.Btn_AddAlarm.Size = new System.Drawing.Size(123, 41);
+            this.Btn_AddAlarm.Size = new System.Drawing.Size(123, 50);
             this.Btn_AddAlarm.TabIndex = 0;
             this.Btn_AddAlarm.Text = "添加";
             this.Btn_AddAlarm.UseVisualStyleBackColor = true;
-            // 
-            // Btn_DeleteAlarm
-            // 
-            this.Btn_DeleteAlarm.FlatAppearance.BorderSize = 0;
-            this.Btn_DeleteAlarm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Btn_DeleteAlarm.Location = new System.Drawing.Point(261, 3);
-            this.Btn_DeleteAlarm.Name = "Btn_DeleteAlarm";
-            this.Btn_DeleteAlarm.Size = new System.Drawing.Size(123, 41);
-            this.Btn_DeleteAlarm.TabIndex = 0;
-            this.Btn_DeleteAlarm.Text = "删除";
-            this.Btn_DeleteAlarm.UseVisualStyleBackColor = true;
             // 
             // Btn_EditAlarm
             // 
             this.Btn_EditAlarm.FlatAppearance.BorderSize = 0;
             this.Btn_EditAlarm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_EditAlarm.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Btn_EditAlarm.Location = new System.Drawing.Point(132, 3);
             this.Btn_EditAlarm.Name = "Btn_EditAlarm";
-            this.Btn_EditAlarm.Size = new System.Drawing.Size(123, 41);
+            this.Btn_EditAlarm.Size = new System.Drawing.Size(123, 50);
             this.Btn_EditAlarm.TabIndex = 0;
             this.Btn_EditAlarm.Text = "编辑";
             this.Btn_EditAlarm.UseVisualStyleBackColor = true;
             // 
+            // Btn_DeleteAlarm
+            // 
+            this.Btn_DeleteAlarm.FlatAppearance.BorderSize = 0;
+            this.Btn_DeleteAlarm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_DeleteAlarm.Font = new System.Drawing.Font("微软雅黑", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Btn_DeleteAlarm.Location = new System.Drawing.Point(261, 3);
+            this.Btn_DeleteAlarm.Name = "Btn_DeleteAlarm";
+            this.Btn_DeleteAlarm.Size = new System.Drawing.Size(123, 50);
+            this.Btn_DeleteAlarm.TabIndex = 0;
+            this.Btn_DeleteAlarm.Text = "删除";
+            this.Btn_DeleteAlarm.UseVisualStyleBackColor = true;
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(911, 498);
             this.Controls.Add(this.FPanel_ToolBox);
+            this.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Name = "MainForm";
             this.Text = "My Alarm";
             this.FPanel_ToolBox.ResumeLayout(false);
